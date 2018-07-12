cbf943ec8009ce55b60fb765dcb0d9d897d999c3
Chris Trevarthen <ctrevarthen@detroitlabs.com>
2018-07-12 00:14:45 -0400

    Add resource-id xpath checking with contains().

--- src/main/java/com/detroitlabs/katalonsupport/android/AndroidTouch.java
+++ src/main/java/com/detroitlabs/katalonsupport/android/AndroidTouch.java
@@ -14,28 +14,38 @@ import io.appium.java_client.TouchAction;
 
 public class AndroidTouch {
 
+	// Xpath 1 (used by Selenium) doesn't have ends-with, so this is a substitute
+	// TODO: Use "contains()"?
+	static String textViewXpathString = "(substring(@class, string-length(@class) - string-length('TextView') + 1) = 'TextView')";
+	
+	// Not all elements have a distinguishable resource-id, so just the text will have to do
 	public static boolean scrollListToElementWithText(String elementText) {
+		return scrollListToElementWithText(null, elementText);
+	}
+	
+	// For Android, elementId equates to the resource-id
+	public static boolean scrollListToElementWithText(String elementId, String elementText) {
 		boolean isElementFound = false;
 		while (isElementFound == false) {
 			try {
 				Logger.debug("Checking for specific element");
 				@SuppressWarnings("unchecked")
 				AppiumDriver<MobileElement> driver = (AppiumDriver<MobileElement>) MobileDriverFactory.getDriver();				
-				driver.findElementByXPath("//android.widget.CheckedTextView[@text='" + elementText + "']");
+				driver.findElementByXPath("//*[" + textViewXpathString + resourceXpathString(elementId) + " and @text='" + elementText + "']");
 				isElementFound = true;
 				Logger.debug("Found one!");
 			} catch (WebDriverException ex) {
 				Logger.debug("Didn't find any matching elements");
-				scrollEntireList();
+				scrollEntireList(elementId);
 			}
 		}
 		return isElementFound;
 	}	
 	
-	private static void scrollEntireList() {
+	private static void scrollEntireList(String elementId) {
 		@SuppressWarnings("unchecked")
 		AppiumDriver<MobileElement> driver = (AppiumDriver<MobileElement>) MobileDriverFactory.getDriver();
-		List<MobileElement> listElement = driver.findElementsByClassName("android.widget.CheckedTextView");
+		List<MobileElement>listElement = driver.findElementsByXPath("//*[" + textViewXpathString + resourceXpathString(elementId) + "]");
 		Logger.debug("Getting list of all elements");
 		TouchAction touchAction = new TouchAction(driver);
 		MobileElement bottomElement = listElement.get(listElement.size() - 1);
@@ -46,6 +56,15 @@ public class AndroidTouch {
 		// Sometimes need a delay after scrolling before checking for the element
 		MobileBuiltInKeywords.delay(5);
 
+	}
+	
+	// Can speed things up to narrow down the collection of elements by resource id
+	private static String resourceXpathString(String elementId) {
+		if (elementId == null) {
+			return "";
+		}
+		
+		return " and contains(@resource-id, '" + elementId + "') ";
 	}
 
 }
