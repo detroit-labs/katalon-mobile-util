0a943ebf209d6d94998abd870b29975f519c2ecf
Chris Trevarthen <ctrevarthen@detroitlabs.com>
2018-08-23 15:10:25 -0400

    Get find a label from a list of labels by index.

--- src/main/java/com/detroitlabs/katalonmobileutil/exception/ListItemsNotFoundException.java
+++ src/main/java/com/detroitlabs/katalonmobileutil/exception/ListItemsNotFoundException.java
@@ -2,12 +2,27 @@ package com.detroitlabs.katalonmobileutil.exception;
 
 import java.util.NoSuchElementException;
 
+import com.detroitlabs.katalonmobileutil.logging.Logger;
+
 public class ListItemsNotFoundException extends NoSuchElementException {
 
 	private static final long serialVersionUID = 1L;
 
 	public ListItemsNotFoundException(String xpath, String resourceId, String resourceType) {
-		super("\nCould not find any list elements matching xpath: " + xpath + "\nCheck that you have elements with " + resourceType + ": '" + resourceId + "'");
+		super(formatMessage(xpath, resourceId, resourceType, null));
 	}
 
+	public ListItemsNotFoundException(String xpath, String resourceId, String resourceType, Integer index) {
+		super(formatMessage(xpath, resourceId, resourceType, index));
+	}
+	
+	protected static String formatMessage(String xpath, String resourceId, String resourceType, Integer index) {
+		String message = "\nCould not find any list elements matching xpath: " + xpath + 
+				"\nCheck that you have elements with " + resourceType + ": '" + resourceId + "'";
+		if (index != null) {
+			message += "\n and that there are at least " + (index + 1) + " elements in the list";
+		}
+		Logger.error(message);
+		return message;
+	}
 }
--- src/main/java/com/detroitlabs/katalonmobileutil/testobject/Finder.java
+++ src/main/java/com/detroitlabs/katalonmobileutil/testobject/Finder.java
@@ -2,8 +2,17 @@ package com.detroitlabs.katalonmobileutil.testobject;
 
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject;
 
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.remote.RemoteWebElement;
+
 import com.detroitlabs.katalonmobileutil.device.Device;
+import com.detroitlabs.katalonmobileutil.exception.ListItemsNotFoundException;
+import com.detroitlabs.katalonmobileutil.logging.Logger;
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory;
 import com.kms.katalon.core.testobject.TestObject;
+import com.kms.katalon.core.testobject.TestObjectProperty;
+
+import io.appium.java_client.AppiumDriver;
 
 public class Finder {
 	
@@ -60,6 +69,63 @@ public class Finder {
 	
 	public static TestObject findGeneric(String name) {
 		return findObject(null, name);
+	}
+	
+	public static TestObject findLabelAtIndex(String testObjectName, int index) {
+		
+		// TODO: should we match the name of the test object or the accessibility id? maybe 2 separate methods?
+				
+		AppiumDriver<?> driver = MobileDriverFactory.getDriver();
+		
+		TestObject templateObject = Finder.findLabel(testObjectName);
+		String resourceId = resourceIdFromTestObject(templateObject);
+		
+		String xpath = "";
+		
+		if (Device.isIOS()) {
+			// The Xcode accessibility id comes through as "name" in the page document
+			xpath = "(//XCUIElementTypeStaticText[@name='" + resourceId + "'])";
+		} else {
+			// Android has multiple classes that count as text views and the "accessibility_id" is resource-id
+			String textViewXpath = "(substring(@class, string-length(@class) - string-length('TextView') + 1) = 'TextView')";
+			// Using "contains" for Android because the resource-id gets appended with package info
+			String resourceIdXpath = "contains(@resource-id, '" + resourceId + "')";
+			xpath = "(//*[" + textViewXpath + " and " + resourceIdXpath + "])";
+			
+		}
+		
+		xpath = xpath + "[" + index + "]";
+		
+		RemoteWebElement listElement = null;
+		
+		Logger.debug("Looking for elements with xpath: " + xpath);
+		try {
+			listElement = (RemoteWebElement) driver.findElementByXPath(xpath);			
+		} catch (WebDriverException ex) {
+			throw(new ListItemsNotFoundException(xpath, resourceId, "accessibility id", index));
+		}
+
+		Logger.debug("Found element:" + listElement);
+		TestObject objectAtIndex = TestObjectConverter.fromElement(listElement);
+		
+		// return the test object
+		return objectAtIndex;
+	}
+	
+	private static String resourceIdFromTestObject(TestObject testObject) {
+		String resourceId = null;
+		
+		// for iOS, accessiblityId will be in the property "name", for Android it's "resource-id"
+		String propertyName = Device.isIOS() ? "name" : "resource-id";
+		
+		for (TestObjectProperty p : testObject.getActiveProperties()) {
+			if (propertyName.equals(p.getName())) {
+				resourceId = p.getValue();
+				break;
+			}
+		}
+		
+		return resourceId;
 	}
 	
 	private static TestObject findObject(String type, String name) {
