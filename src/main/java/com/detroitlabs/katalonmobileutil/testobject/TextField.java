d24e199a6bb30ecfa0ee7541d806f7107c6974c6
Chris Trevarthen <ctrevarthen@detroitlabs.com>
2018-09-11 14:57:14 -0400

    Handle multiple pickers (or just a single text list) in Android.
    Add hideKeyboard and nextField functions to TextField that handle iOS/Android.

--- src/main/java/com/detroitlabs/katalonmobileutil/testobject/TextField.java
+++ src/main/java/com/detroitlabs/katalonmobileutil/testobject/TextField.java
@@ -1,12 +1,13 @@
 package com.detroitlabs.katalonmobileutil.testobject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.interactions.Keyboard;
 
 import com.detroitlabs.katalonmobileutil.device.Device;
-import com.detroitlabs.katalonmobileutil.exception.NoSuchPickerChoiceException;
 import com.detroitlabs.katalonmobileutil.logging.Logger;
 import com.detroitlabs.katalonmobileutil.logging.Logger.LogLevel;
 import com.detroitlabs.katalonmobileutil.touch.Scroll;
@@ -18,6 +19,8 @@ import com.kms.katalon.core.testobject.TestObject;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidKeyCode;
 
 public class TextField {
 	
@@ -81,42 +84,135 @@ public class TextField {
 	}	
 	
 	public static void selectOption(TestObject field, List<String> pickerChoices, Integer timeout) {
+		
+		// Open the picker by tapping on the field that triggers it
 		MobileBuiltInKeywords.tap(field, timeout);
 
-		if (Device.isIOS()) {
-			
-			// Sometimes there will be multiple pickers side-by-side, e.g. multipart date formats for month, date, year
-			for (int i=0; i < pickerChoices.size(); i++) {
-				// Find the picker wheel and set its text, which spins the wheel
-				TestObject pickerWheel = new TestObject();
-				pickerWheel.addProperty("xpath", ConditionType.EQUALS, "(//XCUIElementTypePickerWheel)[" + (i + 1) + "]");
-				MobileBuiltInKeywords.setText(pickerWheel, pickerChoices.get(i), timeout);
-				Logger.debug("Current pickerWheel value: " + MobileBuiltInKeywords.getText(pickerWheel, timeout));
+		// Sometimes there will be multiple pickers side-by-side, e.g. multipart date formats for month, date, year
+		for (int i=0; i < pickerChoices.size(); i++) {
+			String pickerChoice = pickerChoices.get(i);
+		
+			if (Device.isIOS()) {
+				
+				selectOptionFromIOSPicker(i, pickerChoices.get(i), timeout);
+				
+			} else {
+						
+				 	// Android
+					try {
+						selectOptionFromAndroidPicker(i, pickerChoice, timeout);
+					} catch (NoSuchElementException ex) {
+						// TODO: How to handle multiple pickers?
+						// Not a real picker, just a list of text elements
+						selectOptionFromAndroidTextList(pickerChoice, timeout); 
+					}
+
 			}
 			
-			// Find the SELECT or DONE button on the picker wheel toolbar and tap it, applying the value to the field
-			TestObject selectButton = new TestObject();
-			// Xpath 1 (used by Selenium) doesn't have matches, so this is a substitute
-			selectButton.addProperty("xpath", ConditionType.EQUALS, "//XCUIElementTypeButton[@name='SELECT' or @name='DONE']");
-			Logger.debug("Tapping the button to close the picker:");
-			Logger.printTestObject(selectButton, LogLevel.DEBUG);
-			MobileBuiltInKeywords.tap(selectButton, timeout);
-			
-			// TODO: Verify that the value selected was actually set. This can be tricky if the text of the picker gets transformed
-			// in code when selected, e.g. October -> 10
-			// It is possible to try to set the text of a field to something not in the picker list, in which case it fails silently.
-			// We need to verify that the selection was made correctly, or throw an exception.
-//			String newTextFieldValue = MobileBuiltInKeywords.getText(field, timeout);
-//			if (!pickerChoice.equals(newTextFieldValue)) {
-//				throw(new NoSuchPickerChoiceException(pickerChoice));
-//			}
-			
-		} else {
-			// For Android, the picker is a scrolling list of labels
-//			Scroll.scrollListToElementWithText(pickerChoice);
-//			TestObject selection = Finder.findLabelWithText(pickerChoice);
-//			MobileBuiltInKeywords.tap(selection, timeout);
 		}
+		
+		if (Device.isIOS()) {
+			// Find the SELECT or DONE button on the picker wheel toolbar and tap it, applying the value to the field
+			TextField.tapButtonWithNameIn(Arrays.asList("SELECT", "Select", "DONE", "Done"));
+		}
+		
+		// TODO: Verify that the value selected was actually set. This can be tricky if the text of the picker gets transformed
+		// in code when selected, e.g. October -> 10
+		// It is possible to try to set the text of a field to something not in the picker list, in which case it fails silently.
+		// We need to verify that the selection was made correctly, or throw an exception.
+//		String newTextFieldValue = MobileBuiltInKeywords.getText(field, timeout);
+//		if (!pickerChoice.equals(newTextFieldValue)) {
+//			throw(new NoSuchPickerChoiceException(pickerChoice));
+//		}
+		
+	}
+	
+	/**
+	 * Moves the keyboard focus to the next field in the form
+	 */
+	public static void nextField() {
+		// TODO: Check if keyboard is open?
+		if (Device.isIOS()) {
+			// For iOS, look at the buttons and find one that matches "Next", but there can be a few variations
+			TextField.tapButtonWithNameIn(Arrays.asList("NEXT", "Next", ">"));
+		} else {
+			// For Android, we can use the Tab key
+			AndroidDriver<?> driver = (AndroidDriver<?>) MobileDriverFactory.getDriver();
+			driver.pressKeyCode(AndroidKeyCode.KEYCODE_TAB);
+		}
+	}
+	
+	public static void hideKeyboard() {
+		AppiumDriver<?> driver = (AppiumDriver<?>) MobileDriverFactory.getDriver();
+		if (driver.getKeyboard() != null) {
+			if (Device.isIOS()) {
+				// The hideKeyboard function often crashes in iOS.
+				// Find the DONE button on the keyboard toolbar and tap it, closing the keyboard
+				TextField.tapButtonWithNameIn(Arrays.asList("DONE", "Done"));
+			} else {
+				// Android's hideKeyboard seems more reliable.
+				driver.hideKeyboard();
+			}	
+		} else {
+			Logger.debug("Keyboard is already hidden.");
+		}
+		
+	}
+	
+	private static void tapButtonWithNameIn(List<String> names) {
+		TestObject button = new TestObject();
+		// Xpath 1 (used by Selenium) doesn't have matches, so this is a substitute
+		StringBuilder sb = new StringBuilder();
+		sb.append("@name='").append(names.get(0)).append("'");
+		for (int i=1; i<names.size(); i++) {
+			sb.append(" or @name='").append(names.get(i)).append("'");
+		}
+		button.addProperty("xpath", ConditionType.EQUALS, "//XCUIElementTypeButton[" + sb.toString() + "]");
+		Logger.debug("Tapping the button to close the keyboard or picker:");
+		Logger.printTestObject(button, LogLevel.DEBUG);
+		MobileBuiltInKeywords.tap(button, 0);
+	}
+	
+	private static void selectOptionFromAndroidPicker(int pickerIndex, String pickerChoice, int timeout) throws NoSuchElementException {
+		
+		// When the picker is an actual picker, find the picker wheel's EditText node and set its text, which spins the wheel
+		
+		AndroidDriver<?> driver = (AndroidDriver<?>) MobileDriverFactory.getDriver();	
+		
+		// Find the picker at the given index (xpath indexes start at 1)
+		String pickerXPath = "(//android.widget.NumberPicker)[" + (pickerIndex + 1) + "]/android.widget.EditText";
+		
+		driver.findElementByXPath(pickerXPath);
+
+		TestObject picker = new TestObject();
+		picker.addProperty("xpath", ConditionType.EQUALS, pickerXPath);
+		
+		// Need to activate the keyboard first
+		MobileBuiltInKeywords.tap(picker, timeout);
+		
+		// Setting the text will spin the picker
+		MobileBuiltInKeywords.setText(picker, pickerChoice, timeout);
+		
+		// In order for the picker change to take effect, need to apply it to the field
+		driver.pressKeyCode(AndroidKeyCode.ENTER);
+		
+	}
+	
+	private static void selectOptionFromAndroidTextList(String pickerChoice, int timeout) {
+		// When the "picker" is just a list of labels
+		Scroll.scrollListToElementWithText(pickerChoice);
+		TestObject selection = Finder.findLabelWithText(pickerChoice);
+		MobileBuiltInKeywords.tap(selection, timeout);
+	}
+	
+	private static void selectOptionFromIOSPicker(int pickerIndex, String pickerChoice, int timeout) {
+		// Find the picker wheel and set its text, which spins the wheel
+		TestObject pickerWheel = new TestObject();
+		pickerWheel.addProperty("xpath", ConditionType.EQUALS, "(//XCUIElementTypePickerWheel)[" + (pickerIndex + 1) + "]");
+//		MobileBuiltInKeywords.setText(pickerWheel, pickerChoice, timeout);
+		MobileBuiltInKeywords.sendKeys(pickerWheel, pickerChoice);
+		Logger.debug("Current pickerWheel value: " + MobileBuiltInKeywords.getText(pickerWheel, timeout));
+		
 	}
 
 }
--- src/main/java/com/detroitlabs/katalonmobileutil/touch/Scroll.java
+++ src/main/java/com/detroitlabs/katalonmobileutil/touch/Scroll.java
@@ -4,6 +4,13 @@ import com.detroitlabs.katalonmobileutil.device.Device;
 
 public class Scroll {
 
+	/** 
+	 * Scrolls through a list of all text elements on the screen, attempting to find the requested text.
+	 * Throws an exception if the text is not found.
+	 * Android only. For iOS, use scrollListToElementWithText(String elementId, String elementText)
+	 * @param elementText text to attempt to find in the scrolling list.
+	 * @return true if the text was found.
+	 */
 	public static boolean scrollListToElementWithText(String elementText) {
 		if (Device.isIOS()) {
 			throw new UnsupportedOperationException("Parameters elementId and elementText are required for iOS.");
@@ -16,6 +23,13 @@ public class Scroll {
 		throw new UnsupportedOperationException("Device type is not supported.");
 	}
 	
+	/**
+	 * Scrolls through a list of a specific collection of elements on the screen, attempting to find the requested text.
+	 * Throws an exception if the text is not found. 
+	 * @param elementId identifier of the collection of text elements to scroll (Accessibility id/name for iOS and resource-id for Android).
+	 * @param elementText text to attempt to find in the scrolling list.
+	 * @return true if the text was found.
+	 */
 	public static boolean scrollListToElementWithText(String elementId, String elementText) {
 		if (Device.isIOS()) {
 			return ScrollIOS.scrollListToElementWithText(elementId, elementText);
