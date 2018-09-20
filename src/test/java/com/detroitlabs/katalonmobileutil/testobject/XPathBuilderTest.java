c2fa2a9902525dfca986476517e0084fdce4709e
Chris Trevarthen <ctrevarthen@detroitlabs.com>
2018-09-19 22:35:17 -0400

    Bump to JRE 8.
    Add JUnit 5 testing framework.

---
+++ src/test/java/com/detroitlabs/katalonmobileutil/testobject/XPathBuilderTest.java
@@ -0,0 +1,19 @@
+package com.detroitlabs.katalonmobileutil.testobject;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class XPathBuilderTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testThisThing() {
+		fail("Not yet implemented");
+	}
+
+}
