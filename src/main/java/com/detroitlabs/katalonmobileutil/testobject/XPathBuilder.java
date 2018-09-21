f8e35b7f03b51e657d47fdb59a7edf64ab051eb6
Chris Trevarthen <ctrevarthen@detroitlabs.com>
2018-09-21 14:13:57 -0400

    Start adding tests for XPathBuilder.

--- src/main/java/com/detroitlabs/katalonmobileutil/testobject/XPathBuilder.java
+++ src/main/java/com/detroitlabs/katalonmobileutil/testobject/XPathBuilder.java
@@ -1,9 +1,43 @@
 package com.detroitlabs.katalonmobileutil.testobject;
 
+import java.util.List;
+
 import com.detroitlabs.katalonmobileutil.device.Device;
 
 public class XPathBuilder {
 
+	public static String createXPath(String type) {
+		String xpath = "";
+		if (Device.isIOS()) {
+			xpath = "//*[equals(@type, '" + type + "')]";
+		} else {
+			xpath = "//*[contains(@class, '" + type + "')]";
+		}
+		return xpath;
+	}
+	
+	public static String createXPath(List<String> types) {
+		String xpath = "//*[(";
+		
+		for (int i=0; i<types.size(); i++) {
+			
+			if (Device.isIOS()) {
+				xpath = xpath + "equals(@type, '" + types.get(i) + "')";
+			} else {
+				xpath = xpath + "contains(@class, '" + types.get(i) + "')";
+			}
+			
+			if (types.size() > i+1) {
+				xpath = xpath + " or ";
+			}
+			
+		}
+		
+		xpath = xpath + ")]";
+		
+		return xpath;
+	}
+	
 	// TODO: Make this more generic to build xpath for all types?
 	public static String xpathForLabelWithResourceId(String resourceId) {
 		return xpathForLabel(resourceId, null);
@@ -39,6 +73,37 @@ public class XPathBuilder {
 		return xpath;
 	}	
 	
+	public static String xpathForCheckboxWithText(String checkboxText) {
+		String xpath = "";
+		
+		if (Device.isIOS()) {
+			xpath = "//XCUIElementTypeStaticText[";
+		} else {
+			xpath = "//android.widget.CheckBox[";
+		}
+		
+		xpath = xpath + (checkboxText != null ? labelXPath(checkboxText) : "");
+		xpath = xpath + "]";
+
+		return xpath;
+	}
+	
+	public static String addVisible(String xpath) {
+		// find end of the xpath string
+		// check if there are any statements in it already (if so, add an "and")
+		// add a visible statement
+		// TODO: Handle an xpath that is an array
+		return null;
+	}
+	
+	public static String addLabel(String xpath, String label) {
+		// find end of xpath string
+		// check if there are any statements in it already (if so, add an "and")
+		// add a label= statement
+		// TODO: Handle an xpath that is an array
+		return null;
+	}
+	
 	private static String resourceIdXPath(String resourceId) {
 		if (Device.isIOS()) {
 			// The Xcode accessibility id comes through as "name" in the page document
@@ -51,7 +116,7 @@ public class XPathBuilder {
 	
 	private static String textViewXPath() {
 		// Android has multiple classes that count as text views
-		return "(substring(@class, string-length(@class) - string-length('TextView') + 1) = 'TextView')";
+		return "contains(@class, 'TextView')";
 	}	
 	
 	private static String labelXPath(String labelText) {
--- src/test/java/com/detroitlabs/katalonmobileutil/testobject/XPathBuilderTest.java
+++ src/test/java/com/detroitlabs/katalonmobileutil/testobject/XPathBuilderTest.java
@@ -1,19 +1,41 @@
 package com.detroitlabs.katalonmobileutil.testobject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.detroitlabs.katalonmobileutil.device.Device;
+
+@PrepareForTest(Device.class)
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore({"org.mockito.*"})
 class XPathBuilderTest {
 
+	public XPathBuilderTest() {}
+	
 	@BeforeEach
-	void setUp() throws Exception {
+	public void setUp() throws Exception {
 	}
 
 	@Test
-	void testThisThing() {
-		fail("Not yet implemented");
+	@DisplayName("creating an xpath with a single class returns an xpath with a single class")
+	public void creatingXPathWithSingleClass_returnsXPathWithClass() {
+		PowerMockito.mockStatic(Device.class);
+		PowerMockito.when(Device.isIOS()).thenReturn(true);
+		
+		assertEquals("//*[equals(@type, 'XCUITypeButton')]", XPathBuilder.createXPath("XCUITypeButton"));
+	}
+	
+	@Test
+	public void creatingXPathWithMultipleClasses_returnsXPathWithMultipleClasses() {
+//		assertEquals(XPathBuilder.createXPath(["TextView", "EditTextView"]), "//[equals(@class, 'XCUITypeButton']");
 	}
 
 }
