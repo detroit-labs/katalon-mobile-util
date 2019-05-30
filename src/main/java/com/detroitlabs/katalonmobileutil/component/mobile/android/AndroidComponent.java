aed5bd37e35f5e440759769c867c87960273252f
Shawn Mallon <shawn.mallon@detroitlabs.com>
2019-05-30 18:32:36 -0400

    Add AndroidComponent and IOSComponent classes

---
+++ src/main/java/com/detroitlabs/katalonmobileutil/component/mobile/android/AndroidComponent.java
@@ -0,0 +1,11 @@
+package com.detroitlabs.katalonmobileutil.component.mobile.android;
+
+import com.detroitlabs.katalonmobileutil.component.mobile.MobileComponent;
+import com.kms.katalon.core.testobject.TestObject;
+
+public class AndroidComponent extends MobileComponent {
+
+    public AndroidComponent(TestObject testObject) {
+        super(testObject);
+    }
+}
---
+++ src/main/java/com/detroitlabs/katalonmobileutil/component/mobile/ios/IOSComponent.java
@@ -0,0 +1,11 @@
+package com.detroitlabs.katalonmobileutil.component.mobile.ios;
+
+import com.detroitlabs.katalonmobileutil.component.mobile.MobileComponent;
+import com.kms.katalon.core.testobject.TestObject;
+
+public class IOSComponent extends MobileComponent {
+
+    public IOSComponent(TestObject testObject) {
+        super(testObject);
+    }
+}
